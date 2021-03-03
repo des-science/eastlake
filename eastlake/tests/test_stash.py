515f7cb647d01e709815fa60fc589e6e75b99098
Matthew R. Becker <beckermr@users.noreply.github.com>
2021-03-03 14:34:14 -0600

    TST fixed step tests

--- eastlake/tests/test_stash.py
+++ eastlake/tests/test_stash.py
@@ -1,15 +1,17 @@
+import os
+
 from ..stash import Stash
 
 
 def test_stash_state():
     stsh = Stash("blah", ["foo", "bar"])
-    assert stsh["base_dir"] == "blah"
+    assert stsh["base_dir"] == os.path.abspath("blah")
     assert stsg["step_names"] == ["foo", "bar"]
     
 
 def test_stash_state_update():
     stsh1 = Stash("blah1", ["foo1", "bar1"])
     stsh2 = Stash("blah2", ["foo2", "bar2"], stash=stsh1)
-    assert stsh["base_dir"] == "blah1"
-    assert stsg["step_names"] == ["foo1", "bar1"]
+    assert stsh2["base_dir"] == os.path.abspath("blah1")
+    assert stsh2["step_names"] == ["foo1", "bar1"]
   
