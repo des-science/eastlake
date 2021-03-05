cda32275ee5dc96b250e85b7392e93399c90bf7d
Masaya Yamamoto <masayayamamoto@masayas-air.home>
2021-03-05 14:26:54 -0500

    added test_stash_io()

--- eastlake/tests/test_stash.py
+++ eastlake/tests/test_stash.py
@@ -5,12 +5,12 @@ import tempfile
 from ..stash import Stash
 
 
-test_dir = os.getcwd() + '/'
+TEST_DIR = os.getcwd() + '/'
 
 
 def test_stash_state():  # test from line 30-35.
     stsh = Stash("blah", ["foo", "bar"])
-    assert os.path.abspath("blah") == test_dir + 'blah'
+    assert os.path.abspath("blah") == TEST_DIR + 'blah'
     assert stsh["base_dir"] == os.path.abspath("blah")
     assert stsh["step_names"] == ["foo", "bar"]
     assert stsh['completed_step_names'] == []
@@ -43,11 +43,11 @@ def test_stash_get_abs_path():  # test from line 53-60.
     # When band==None
     paths = stsh2.get_abs_path('key2', 'tile2', band=None)
     # assert, do we need a test for os.path.isabs() ?
-    assert paths == test_dir + 'blah2/path_to_file2'
+    assert paths == TEST_DIR + 'blah2/path_to_file2'
 
     # When band!=None
     paths = stsh2.get_abs_path('key1', 'tile1', band='r')
-    assert paths == test_dir + 'blah2/path_to_file1'
+    assert paths == TEST_DIR + 'blah2/path_to_file1'
 
 
 def test_stash_set_filepaths():  # test from line 62-86.
@@ -65,7 +65,7 @@ def test_stash_set_filepaths():  # test from line 62-86.
 
     # when filepaths argument is not list. & band is None.
     # filepaths is absolute.
-    stsh2.set_filepaths('key2', test_dir + 'blah2/filepaths2', 'tile2', band=None)
+    stsh2.set_filepaths('key2', TEST_DIR + 'blah2/filepaths2', 'tile2', band=None)
     assert stsh2['tile_info']['tile2']['key2'] == 'filepaths2'
     # filepaths is already relative.
     stsh2.set_filepaths('key2', 'filepaths2', 'tile2', band=None)
@@ -73,14 +73,14 @@ def test_stash_set_filepaths():  # test from line 62-86.
 
     # when filepaths argument is not list. & band is not None.
     # filepaths is absolute.
-    stsh2.set_filepaths('key1', test_dir + 'blah2/filepaths1', 'tile1', band='r')
+    stsh2.set_filepaths('key1', TEST_DIR + 'blah2/filepaths1', 'tile1', band='r')
     assert stsh2['tile_info']['tile1']['r']['key1'] == 'filepaths1'
     # filepaths is already relative.
     stsh2.set_filepaths('key1', 'filepaths1', 'tile1', band='r')
     assert stsh2['tile_info']['tile1']['r']['key1'] == 'filepaths1'
 
     # when filepaths argumet is list. & band is None.
-    paths_list = [test_dir + 'blah2/paths1', test_dir + 'blah2/paths2']
+    paths_list = [TEST_DIR + 'blah2/paths1', TEST_DIR + 'blah2/paths2']
     # filepaths is absolute.
     stsh2.set_filepaths('key2', paths_list, 'tile2', band=None)
     assert stsh2['tile_info']['tile2']['key2'] == ['paths1', 'paths2']
@@ -90,7 +90,7 @@ def test_stash_set_filepaths():  # test from line 62-86.
     assert stsh2['tile_info']['tile2']['key2'] == ['paths1', 'paths2']
 
     # when filepaths argumet is list. & band is not None.
-    paths_list = [test_dir + 'blah2/paths1', test_dir + 'blah2/paths2']
+    paths_list = [TEST_DIR + 'blah2/paths1', TEST_DIR + 'blah2/paths2']
     stsh2.set_filepaths('key1', paths_list, 'tile1', band='r')  # filepaths is absolute.
     assert stsh2['tile_info']['tile1']['r']['key1'] == ['paths1', 'paths2']
     paths_list = ['paths1', 'paths2']
@@ -149,14 +149,14 @@ def test_stash_get_filepaths():  # test from line 88-123.
         arguments = {
             'str': {
                 'rel': 'paths1',
-                'abs': test_dir + 'blah2/paths1'},
+                'abs': TEST_DIR + 'blah2/paths1'},
             'list': {
                 'rel': [
                     'paths1',
                     'paths2'],
                 'abs': [
-                    test_dir + 'blah2/paths1',
-                    test_dir + 'blah2/paths2']}}
+                    TEST_DIR + 'blah2/paths1',
+                    TEST_DIR + 'blah2/paths2']}}
         for i in arguments.keys():
             for j in ['rel', 'abs']:
                 if band is None:
@@ -177,16 +177,20 @@ def test_stash_get_filepaths():  # test from line 88-123.
 
 
 def test_stash_io():
+    stsh = Stash("blah1", ["foo1", "bar1"])
     with tempfile.TemporaryDirectory() as tmpdir:
         pth = os.path.join(tmpdir, "blah.pkl")
-        # write stash to disk
-        
-        # read stash back
-        
-        # check that they are equal
-        assert stash1 == stash2
-        # assert np.array_equal(np.array([1, 2]), np.array([1, 2]))
-        # assert np.allclose(np.array([1, 2]), np.array([1, 2]))
-
-
-# def test_stash_load():
+        for ow in [False, True]:
+            # write stash to disk
+            stsh.save(pth, overwrite=ow)
+            # read stash back
+            loaded_stsh = stsh.load(pth, TEST_DIR + 'blah1', ["foo1", "bar1"])
+            # check that they are equal
+            assert loaded_stsh == stsh
+            # assert np.array_equal(np.array([1, 2]), np.array([1, 2]))
+            # assert np.allclose(np.array([1, 2]), np.array([1, 2]))
+            # testing overwrite after the file is made.
+            if not ow:
+                with pytest.raises(IOError) as e:
+                    stsh.save(pth, overwrite=ow)
+                    assert e.type is IOError
