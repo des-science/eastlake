bf075752edd182f92b13280c50ca3d717cdeb481
Masaya Yamamoto <54725989+myamamoto26@users.noreply.github.com>
2021-03-11 12:27:43 -0500

    Removed from ._ngmixer line

--- eastlake/steps/__init__.py
+++ eastlake/steps/__init__.py
@@ -2,7 +2,6 @@ from ._meds import MEDSRunner  # noqa
 from ._galsim import GalSimRunner  # noqa
 from .swarp import SingleBandSwarpRunner, SWarpRunner  # noqa
 from .source_extractor import SExtractorRunner  # noqa
-from ._ngmixer import MOFRunner, SOFRunner, McalRunner  # noqa
 from .delete_images import DeleteImages  # noqa
 from .true_detection import TrueDetectionRunner  # noqa
 from .delete_meds import DeleteMeds  # noqa
