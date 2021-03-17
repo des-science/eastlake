afa7c0a3a09b91d351ae8619e926aa649c8fb3dd
Masaya Yamamoto <masayayamamoto@masayas-air.home>
2021-03-17 11:21:44 -0400

    pushing to test pipeline branch

--- eastlake/tests/test_pipeline.py
+++ eastlake/tests/test_pipeline.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import pytest
-from unittest import TestCase
 import tempfile
 import logging
 from collections import OrderedDict
@@ -9,68 +8,331 @@ from collections import OrderedDict
 from ..steps import *
 from ..step import Step
 from ..stash import Stash
-from ..pipeline import Pipeline
+from ..pipeline import Pipeline, DEFAULT_STEPS
 
 
 TEST_DIR = os.getcwd()
-STEP_CLASSES = OrderedDict( [ ('galsim', GalSimRunner),
-                              ('swarp', SWarpRunner),
-                              ('sextractor', SExtractorRunner),
-                              ('meds', MEDSRunner),
-                              ('single_band_swarp', SingleBandSwarpRunner),
-                              ('true_detection', TrueDetectionRunner),
-                              ('delete_images', DeleteImages),
-                              ('delete_meds', DeleteMeds),
-                            ] )
-DEFAULT_STEPS = ["galsim", "swarp", "sextractor", "meds"]
+CONFIG = """\
+modules:
+    - galsim.des
+    - galsim_extra
+    - montara
+    - numpy
+
+pipeline:
+    steps: [galsim, single_band_swarp, swarp, sextractor, meds, delete_images, mcal, delete_meds]
+
+delete_images:
+    delete_coadd: True
+    delete_se: True
+    save_tilenames:
+    - DES0003-3832
+
+desy3cols:
+  shear_weight_grid: ${DESDATA}/y3_shape_w_grid_03_16_20_highsnr.txt
+
+delete_meds:
+    save_tilenames: [DES0003-3832]
+
+all_tile_cats:
+    tag:
+
+meds:
+    cutout_types: ['image','weight','seg','bmask']
+    meds_dir: meds
+    meds_run: y3v02
+    sub_bkg: False
+    add_psf_data: True
+    use_joblib: True
+
+sof:
+    config_file: ${IMSIM_DIR}/ngmix_config/run-y3imsim-sof-psfinfile.yaml
+    clobber: True
+    use_joblib: True
+
+mcal:
+    config_file: ${IMSIM_DIR}/ngmix_config/run-y3imsim-mcal-nocorrect-psfinfile.yaml
+    clobber: True
+    use_joblib: True
+
+single_band_swarp:
+    config_file: ${IMSIM_DIR}/astromatic_config/Y3A1_v1_swarp.config
+    swarp_cmd: swarp
+    ref_mag_zp: 30.
+    update:
+        NTHREADS: 8
+        PIXEL_SCALE : 0.263
+        IMAGE_SIZE : 10000,10000
+
+swarp:
+    config_file: ${IMSIM_DIR}/astromatic_config/Y3A1_v1_swarp.config
+    swarp_cmd: swarp
+    center_from_header: True
+    coadd_bands: ['r','i','z']
+    mask_hdu : 1
+    update:
+        RESAMPLE : N
+        COPY_KEYWORDS : BUNIT,TILENAME,TILEID
+        PIXEL_SCALE : 0.263
+        IMAGE_SIZE : 10000,10000
+        COMBINE_TYPE : CHI-MEAN
+        NTHREADS : 32
+        BLANK_BADPIXELS : Y
+
+#Options for SExtractor - can just provide a sextractor config file as below
+#Fields can be updated using the update section e.g. here we update the detection
+#threshold DETECT_THRESH.
+sextractor:
+    sex_cmd: sex
+    #single_band_det: r
+    config_file: ${IMSIM_DIR}/astromatic_config/Y3A1_v1_sex.config
+    params_file : ${IMSIM_DIR}/astromatic_config/deblend.param
+    filter_file : ${IMSIM_DIR}/astromatic_config/Y3A1_v1_gauss_3.0_7x7.conv
+    star_nnw_file : ${IMSIM_DIR}/astromatic_config/Y3A1_v1_sex.nnw
+    update:
+        CHECKIMAGE_TYPE : SEGMENTATION,BACKGROUND,BACKGROUND_RMS
+        DEBLEND_MINCONT : 0.001
+        DETECT_THRESH : 1.1
+        ANALYSIS_THRESH : 1.1
+
+eval_variables:
+    srun: e2e-test
+    sstar_mag_col: &star_mag_col
+        type: FormattedStr
+        format: "mag_%s"
+        items:
+        - "$band"
+    sgal_mag_col: &gal_mag_col
+        type: FormattedStr
+        format: "mag_%s_dered"
+        items:
+        - "$band"
+    ftruth_g:
+        type: List
+        items:
+        - 0.
+        - '$float((@gal.items.0.ellip).g)'
+        index: '@current_obj_type_index'
+
+    ftruth_beta:
+        type: List
+        items:
+        - 0.
+        - '$float((@gal.items.0.ellip).beta.rad)'
+        index: '@current_obj_type_index'
+    sz_col: &z_col "photoz"
+
+input:
+    # Use analytic galaxies with size and flux parameters that match the distribution seen
+    # in the COSMOS galaxies.
+    catalog_sampler:
+        file_name: /global/project/projectdirs/des/y3-image-sims/input_cosmos_v4.fits
+        cuts:
+            mag_i: [15., 25.]  #use only 15<mag_i<25. for now.
+            isgal: [1,] #select galaxies only since we're simulating stars separately.
+            mask_flags: [0,] #apply mask flags
+            bdf_hlr: [0.,5.]
+    desstar:
+        file_name:
+            type: FormattedStr
+            format: /global/cscratch1/sd/maccrann/DES/image_sims/star_cats_v0/stars-%s.fits
+            items:
+            - "$tilename"
+        mag_i_max: 25.
+    des_piff:
+        file_name: "$piff_path"
+
+image:
+    type: WideScattered
+    border: 15
+    random_seed: 1234
+    nproc: 1
+
+    # The number of objects across the full focal plane.
+    nobjects:
+        type: MixedNObjects
+        ngalaxies:
+            type: RandomPoisson
+            mean: 170000
+        use_all_stars: True
+
+    #could read this from the image headers, but let's set them explicitly for now
+    xsize: 2048
+    ysize: 4096
+
+    world_pos:
+        type: RADec
+        type: RADec
+        ra:
+            type: Degrees
+            theta: { type: Random, min: "$ra_min_deg", max: "$ra_max_deg" }
+        dec:
+            type: Radians
+            theta:
+                type: RandomDistribution
+                function: "math.cos(x)"  # Uniform on the sky means P(delta) ~ cos(delta)
+                x_min: "$numpy.radians(dec_min_deg)"
+                x_max: "$numpy.radians(dec_max_deg)"
+
+#use Piff PSF for now
+psf:
+    type: DES_Piff
+    use_substitute: "$is_blacklisted"
+    no_smooth: False
+    substitute_psf:
+        type: Moffat
+        beta: 3.
+        fwhm: 1.
+
+#No knots
+gal:
+    type: Sum
+    items:
+        - type: Exponential
+          half_light_radius: { type: catalog_sampler_value, col: bdf_hlr }
+          ellip:
+              type: GBeta
+              g: { type: Eval, str: "np.sqrt(g1**2 + g2**2)", fg1: { type: catalog_sampler_value, col: bdf_g1 }, fg2: { type: catalog_sampler_value, col: bdf_g2 } }
+              beta: { type: Random }
+          flux: { type: Eval, str: "1-fracdev", ffracdev: { type: catalog_sampler_value, col: bdf_fracdev } }
+
+        - type: DeVaucouleurs
+          half_light_radius: '@gal.items.0.half_light_radius'
+          ellip: "@gal.items.0.ellip"
+          flux: "$1-@gal.items.0.flux"
+
+    flux:
+        type: Eval
+        #Input catalog has mag
+        #convert to flux via flux = 10**(0.4*(mag_zp-mag))
+        str: "10**(0.4*(mag_zp-mag))"
+        fmag: { type: catalog_sampler_value, col: *gal_mag_col }
+
+star:
+    type: Gaussian  # Basically a delta function.
+    sigma: 1.e-6
+    flux:
+        type: Eval
+        str: "10**( 0.4 * (mag_zp - mag))"
+        fmag: { type: DESStarValue, col: *star_mag_col }
+
+stamp:
+    type: MixedScene
+    objects:
+        # These give the probability of picking each kind of object.  The
+        # choice of which one is picked for a given object is written to the
+        # base dict as base['current_obj_type'] and is thus available as
+        # @current_obj_type.  The actual constructed object is similarly
+        # available as @current_obj.  And the type by number in this list
+        # (starting with 0 for the first) is @current_obj_type_index.
+        star: 0.2
+        gal: 0.8
+    obj_type: {type: Eval,
+              str: "object_type_list[i]",
+              ii: "$obj_num-start_obj_num"
+              }
+    draw_method: auto
+    shear:
+        type: G1G2
+        g1: 0.02
+        g2: 0.00
+    gsparams:
+        maximum_fft_size: 16384
+
+output:
+    type: DESTile
+    nproc: 32
+    # The number of exposures to build
+    bands: [g,r,i,z]
+    desrun: y3v02
+    desdata: /global/project/projectdirs/des/y3-image-sims
+    noise_mode: from_weight
+    add_bkg: False
+    tilename: DES0003-3832
+    blacklist_file: /global/homes/m/maccrann/DES/y3-wl_image_sims/input/piff_stuff/blacklist400.yaml
+
+    #Save weight and badpix extensions too
+    badpixfromfits:
+        hdu: 1
+        mask_hdu: 2
+        mask_file: "$orig_image_path"
+    weight:
+        hdu: 2
+
+    truth:
+        #DESTile type fills in filename
+        columns:
+            num: obj_num
+            half_light_radius:
+                type: Eval
+                str: "0.0 if @current_obj_type=='star' else hlr"
+                fhlr: "@gal.items.0.half_light_radius"
+            g1: "$(@stamp.shear).g1"
+            g2: "$(@stamp.shear).g2"
+            g: "$truth_g"
+            beta: "$truth_beta"
+            obj_type: "@current_obj_type"
+            obj_type_index: "@current_obj_type_index"
+            band: "band"
+            mag_zp: "$mag_zp"
+            laigle_number:
+                type: Eval
+                str: "-1 if @current_obj_type=='star' else int(laigle_number)"
+                flaigle_number: { type: catalog_sampler_value, col: laigle_number }
+            z:
+                type: Eval
+                str: "-1. if @current_obj_type=='star' else z_gal"
+                fz_gal: { type: catalog_sampler_value, col: "$z_col" }
+"""
 
 
 
 def test_pipeline_state(capsys): 
-	base_dir = os.path.join(TEST_DIR,'foo')
-	config = None
-	steps = []
-	for step_name in DEFAULT_STEPS:
-		steps.append(Step(config, base_dir, name=step_name, logger=None, verbosity=None, log_file=None))
-	pl = Pipeline(steps, base_dir, logger=None, verbosity=1, log_file=None, name="pipeline", config=None, record_file="job_record.pkl")
+	with tempfile.TemporaryDirectory() as tmpdir:
+		base_dir = tmpdir #os.path.join(TEST_DIR,'foo')
+		config = None
+		steps = []
+		for step_name in DEFAULT_STEPS:
+			steps.append(Step(config, base_dir, name=step_name, logger=None, verbosity=None, log_file=None))
+		pl = Pipeline(steps, base_dir, logger=None, verbosity=1, log_file=None, name="pipeline", config=None, record_file="job_record.pkl")
 
-	assert pl.name == 'pipeline' 
-	# calling get_logger(). check for print statements
-	captured = capsys.readouterr()
-	assert "log_file=" in captured.out 
-	assert "filemode=" in captured.out 
-	logging.basicConfig(format="%(message)s", level=logging.WARNING, stream=sys.stdout, filemode='w') 
-	log1 = logging.getLogger(pl.name) 
-	assert pl.logger == log1
+		assert pl.name == 'pipeline' 
+		# calling get_logger(). check for print statements
+		captured = capsys.readouterr()
+		assert "log_file=" in captured.out 
+		assert "filemode=" in captured.out 
+		logging.basicConfig(format="%(message)s", level=logging.WARNING, stream=sys.stdout, filemode='w') 
+		log1 = logging.getLogger(pl.name) 
+		assert pl.logger == log1
 
-	# logger is not None to start. Create pl2. 
-	pl2 = Pipeline(steps, base_dir, logger=log1, verbosity=1, log_file=None, name="pipeline", config=None, record_file="job_record.pkl")
-	assert pl2.logger == log1
+		# logger is not None to start. Create pl2. 
+		pl2 = Pipeline(steps, base_dir, logger=log1, verbosity=1, log_file=None, name="pipeline", config=None, record_file="job_record.pkl")
+		assert pl2.logger == log1
 
-	# base_dir not None.
-	assert os.path.isdir(base_dir)
-	assert pl.base_dir == os.path.join(TEST_DIR, 'foo')
-	# base_dir is None. 
-	assert Pipeline(steps, None, logger=None, verbosity=1, log_file=None, name="pipeline", config=None, record_file="job_record.pkl").base_dir == TEST_DIR
+		# base_dir not None.
+		assert os.path.isdir(base_dir)
+		assert pl.base_dir == tmpdir #os.path.join(TEST_DIR, 'foo')
+		# base_dir is None. When base_dir is None, base_dir should be created as cwd. 
+		assert Pipeline(steps, None, logger=None, verbosity=1, log_file=None, name="pipeline", config=None, record_file="job_record.pkl").base_dir == TEST_DIR
 
-	# test logging error. => ignore for now. 
+		# test logging error. => ignore for now. 
 
-	# steps, step_name test. Use pl. 
-	assert pl.steps == steps
-	assert pl.step_names == [s.name for s in steps]
+		# steps, step_name test. Use pl. 
+		assert pl.steps == steps
+		assert pl.step_names == [s.name for s in steps]
 
-	# if config is not None. Create pl3. 
-	pl3 = Pipeline(steps, base_dir, logger=log1, verbosity=1, log_file=None, name="pipeline", config='bar', record_file="job_record.pkl")
-	# check if config is dumped into f. => skip for now?
+		# if config is not None. Create pl3. 
+		pl3 = Pipeline(steps, base_dir, logger=log1, verbosity=1, log_file=None, name="pipeline", config='bar', record_file="job_record.pkl")
+		# check if config is dumped into f. => skip for now?
 
-	# record_file check. Use pl. 
-	assert pl.record_file == os.path.join(base_dir, 'job_record.pkl')
-	assert Pipeline(steps, base_dir, logger=None, verbosity=1, log_file=None, name="pipeline", config=None, record_file=base_dir+'/job_record.pkl').record_file == os.path.join(base_dir, 'job_record.pkl')
-	assert Pipeline(steps, base_dir, logger=None, verbosity=1, log_file=None, name="pipeline", config=None, record_file=None).record_file == os.path.join(base_dir, 'job_record.pkl')
+		# record_file check. Use pl. 
+		assert pl.record_file == os.path.join(base_dir, 'job_record.pkl')
+		assert Pipeline(steps, base_dir, logger=None, verbosity=1, log_file=None, name="pipeline", config=None, record_file=base_dir+'/job_record.pkl').record_file == os.path.join(base_dir, 'job_record.pkl')
+		assert Pipeline(steps, base_dir, logger=None, verbosity=1, log_file=None, name="pipeline", config=None, record_file=None).record_file == os.path.join(base_dir, 'job_record.pkl')
 
-	# init stash. Use pl. 
-	assert pl.stash == Stash(base_dir, [s.name for s in steps])
-
+		# init stash. Use pl. 
+		assert pl.stash == Stash(base_dir, [s.name for s in steps])
+"""
 def test_pipeline_from_record_file():
 
 	# parameters. 
@@ -83,35 +345,36 @@ def test_pipeline_from_record_file():
 
 	# when starting from the previous run. logger=None, record_file=None, base_dir=None. 
 	# What should I test here? 
-	pl = Pipeline(steps, base_dir, logger=None, verbosity=1, log_file=None, name="pipeline", config=None, record_file=None)
-	pipe_cont = pl.from_record_file(config_file, job_record_file, base_dir=None, logger=None, verbosity=1, log_file=None, name="pipeline_cont", step_names=steps, new_params=None, record_file=None)
+	pipe_cont = Pipeline.from_record_file(config_file, job_record_file, base_dir=None, logger=None, verbosity=1, log_file=None, name="pipeline_cont", step_names=steps, new_params=None, record_file=None)
 	stsh = Stash.load()
+"""
 
 def test_pipeline_from_config_file():
 
-	config_file = './eastlake/e2e-008_test.yaml'
-	job_record_file = 'job_record.pkl'
-	base_dir = os.path.join(TEST_DIR,'foo')
+	with tempfile.TemporaryDirectory() as tmpdir:
+		with tempfile.TemporaryFile(mode="w+") as tmp_config_file:
+			tmp_config_file.writelines(CONFIG)
+			tmp_config_file.seek(0)
+			job_record_file = 'job_record.pkl'
+			base_dir = tmpdir
 
-	pl = Pipeline(steps, base_dir, logger=None, verbosity=1, log_file=None, name="pipeline", config=None, record_file=None)
-	pipe_conf = pl.from_conf_file(config_file, base_dir, logger=None, verbosity=1, log_file=None, name="pipeline", step_names=None, new_params=None, record_file=None)
-	assert pipe_conf.logger == logging.getLogger('pipeline') # logger is None. 
-	# len(config) is 1. 
-	assert pipe_conf.config == galsim.config.ReadConfig(config_file)[0]
-	# multiple config files. 
-	with pytest.raises(AssertionError) as e:
-        pipe_conf2 = pl.from_conf_file(['config.yaml', 'config2.yaml'], base_dir, logger=None, verbosity=1, log_file=None, name="pipeline", step_names=None, new_params=None, record_file=None)
-        assert e.type is AssertionError
+			pipe_conf = Pipeline.from_config_file(tmp_config_file, base_dir, logger=None, verbosity=1, log_file=None, name="pipeline", step_names=None, new_params=None, record_file=None)
+			assert pipe_conf.logger == logging.getLogger('pipeline') # logger is None. 
+			# len(config) is 1. 
+			assert pipe_conf.config == galsim.config.ReadConfig(tmp_config_file)[0]
+			# multiple config files. 
+			#with pytest.raises(AssertionError) as e:
+		    #    pipe_conf2 = Pipeline.from_config_file([tmp_config_file, 'config2.yaml'??], base_dir, logger=None, verbosity=1, log_file=None, name="pipeline", step_names=None, new_params=None, record_file=None)
+		    #    assert e.type is AssertionError
 
-    # template is in config.
-    assert pipe_conf.config_dirname == './eastlake'
-    # cwd is not where template is and there is no template in cwd. 
-    assert pipe_conf.template_file_to_use == './eastlake/e2e-008.yaml'
-    assert pipe_conf.config['template'] == './eastlake/e2e-008.yaml'
+		    # template is in config.
+			assert pipe_conf.config_dirname == tmpdir
+			# cwd is not where template is and there is no template in cwd. 
+			assert pipe_conf.template_file_to_use == os.path.join(tmpdir, tmp_config_file)
+			assert pipe_conf.config['template'] == os.path.join(tmpdir, tmp_config_file)
 
-    ## I dont know what to do for line 176-180. 
+		    ## I dont know what to do for line 176-180. 
 
-    # 
 
 
 
