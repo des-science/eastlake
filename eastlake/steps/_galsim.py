a3068798fce78e84a93014898f6b0865690db59b
Masaya Yamamoto <masayayamamoto@masayas-air.home>
2021-04-06 15:12:59 -0400

    added basic galsimrunner, not montaragalsimrunner

--- eastlake/steps/_galsim.py
+++ eastlake/steps/_galsim.py
@@ -438,3 +438,75 @@ class MontaraGalSimRunner(Step):
         self.base_dir = base_dir
         # Update the output directory.
         self.config['output']['dir'] = base_dir
+
+class GalSimRunner(Step):
+    """
+    Pipeline step which runs galsim
+
+    The config attribute is a little different here, since it is updated when
+    running GalSim
+    """
+    def __init__(self, config, base_dir, name="galsim", logger=None,
+                 verbosity=0, log_file=None):
+        super().__init__(
+            config, base_dir, name=name, logger=logger, verbosity=verbosity,
+            log_file=log_file)
+        self.config['output']['dir'] = base_dir
+
+        # For the downstream interpretation of these sims, it's going to be
+        # quite important to have various quantities saved in the truth files.
+        # And that the column names correspond to what we expect them to...so
+        # do a bit of enforcement of that here.
+        output = self.config["output"]
+
+        self.config_orig = galsim.config.CopyConfig(self.config)
+
+    def execute(self, stash, new_params=None, except_abort=False, verbosity=1.,
+                log_file=None, comm=None):
+
+        if comm is not None:
+            rank = comm.Get_rank()
+        else:
+            rank = 0
+
+        if new_params is not None:
+            galsim.config.UpdateConfig(self.config, new_params)
+
+        # Make a copy of original config
+        config = galsim.config.CopyConfig(self.config)
+        if rank == 0:
+            self.logger.debug(
+                "Process config dict: \n%s", pprint.pformat(config))
+
+        if self.name not in stash:
+            stash[self.name] = {}
+
+        galsim.config.Process(config, self.logger, except_abort=except_abort)
+
+        self.update_stash(config, stash)
+
+        # Return status and stash
+        return 0, stash
+
+    def update_stash(self, config, stash):
+        # Update the stash with information on image files etc. required by
+        # following steps.
+
+        # Get the output type and number of files
+        image_type = config["output"]["type"]
+
+        # Add the PSF config
+        stash["psf_config"] = config["psf"]
+        # add draw_method if present
+
+
+    @classmethod
+    def from_config_file(cls, config_file, logger=None):
+        all_config = galsim.config.ReadConfig(config_file, None, logger)
+        assert len(all_config) == 1
+        return cls(all_config[0], logger=logger)
+
+    def set_base_dir(self, base_dir):
+        self.base_dir = base_dir
+        # Update the output directory.
+        self.config['output']['dir'] = base_dir
