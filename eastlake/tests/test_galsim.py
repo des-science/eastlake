24b53a2283805e12d946aefadda07b117810a1cb
Masaya Yamamoto <masayayamamoto@masayas-air.home>
2021-04-07 13:30:41 -0400

    basic galsim test

--- eastlake/tests/test_galsim.py
+++ eastlake/tests/test_galsim.py
@@ -1,14 +1,11 @@
 import os
-import sys
 import tempfile
 import logging
-import yaml
 import galsim
+import fitsio as fio
 
 from ..steps import GalSimRunner
-from ..step import Step
 from ..stash import Stash
-from ..pipeline import Pipeline, DEFAULT_STEPS
 
 
 TEST_DIR = os.getcwd()
@@ -45,9 +42,9 @@ image :
 
     # If you want noise in the image (which is typical) you specify that here.
     # In this case we use gaussian noise.
-    noise :
-        type : Gaussian
-        sigma : 30  # standard deviation of the counts in each pixel
+    #noise :
+    #    type : Gaussian
+    #    sigma : 30  # standard deviation of the counts in each pixel
 
     # You can also specify the size of the image if you want, but if you omit it
     # (as we do here), then GalSim will automatically size the image appropriately.
@@ -63,6 +60,7 @@ output :
     file_name : demo1.fits
 """
 
+
 def test_galsim_execute():
     with tempfile.TemporaryDirectory() as tmpdir:
         base_dir = tmpdir
@@ -70,55 +68,46 @@ def test_galsim_execute():
         with open(config_file_path, "w") as fp:
             fp.write(DEMO1_CONFIG)
 
-        # creating galsim step. Similar to from_config_file() in pipeline.py. 
+        # creating galsim step. Similar to from_config_file() in pipeline.py.
         config = galsim.config.ReadConfig(config_file_path)[0]
         galsim_logger = logging.getLogger("galsim_test")
         step_galsim = [GalSimRunner(config, base_dir, logger=galsim_logger)]
 
-        # execute galsim step. 
+        # execute galsim step.
         galsim_stash = Stash(base_dir, ["galsim"])
         status, stsh = step_galsim[0].execute_step(galsim_stash, new_params=None)
 
-        assert os.path.isfile(os.path.join(base_dir, 'demo1.fits'))
+        obj_file = os.path.join(base_dir, 'demo1.fits')
+        assert os.path.isfile(obj_file)
+
+        # check if the object is drawn.
+        obj = fio.FITS(obj_file)[-1].read()
+        gal_model = galsim.Gaussian(sigma=2, flux=1.e5)
+        psf_model = galsim.Gaussian(sigma=1)
+        gal_model = galsim.Convolve(gal_model, psf_model)
+        gal_stamp = galsim.Image(scale=0.2)
+        gal_model.drawImage(image=gal_stamp)
+        assert obj.all() == gal_stamp.array.all()
 
 
+def test_galsim_from_config_file():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        base_dir = tmpdir
+        config_file_path = os.path.join(tmpdir, "cfg.yaml")
+        with open(config_file_path, "w") as fp:
+            fp.write(DEMO1_CONFIG)
 
+        gal_test = GalSimRunner.from_config_file(config_file_path, base_dir, logger=None)
+        stsh = Stash(base_dir, ["galsim"])
+        gal_test.execute(stsh)
+        obj_file = os.path.join(base_dir, 'demo1.fits')
+        assert os.path.isfile(obj_file)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # check if the object is drawn.
+        obj = fio.FITS(obj_file)[-1].read()
+        gal_model = galsim.Gaussian(sigma=2, flux=1.e5)
+        psf_model = galsim.Gaussian(sigma=1)
+        gal_model = galsim.Convolve(gal_model, psf_model)
+        gal_stamp = galsim.Image(scale=0.2)
+        gal_model.drawImage(image=gal_stamp)
+        assert obj.all() == gal_stamp.array.all()
