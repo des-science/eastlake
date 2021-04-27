8af78a178df293a2c9283d58d97f71249acf825b
Matthew R. Becker <becker.mr@gmail.com>
2021-04-27 09:04:04 -0500

    WIP working swarp

--- setup.py
+++ setup.py
@@ -1,6 +1,53 @@
 import os
+import tempfile
+import contextlib
+import subprocess
 import glob
 from setuptools import setup, find_packages
+import setuptools.command.build_ext
+import setuptools.command.build_py
+
+
+# https://stackoverflow.com/questions/6194499/pushd-through-os-system
+@contextlib.contextmanager
+def pushd(new_dir):
+    previous_dir = os.getcwd()
+    os.chdir(new_dir)
+    try:
+        yield
+    finally:
+        os.chdir(previous_dir)
+
+
+def _run_shell(cmd):
+    print(">>>", cmd, flush=True)
+    subprocess.run(cmd, shell=True, check=True)
+
+
+def _build_swarp():
+    cwd = os.path.abspath(os.getcwd())
+    with tempfile.TemporaryDirectory() as tmpdir:
+        with pushd(tmpdir):
+            _run_shell("cp %s/src/swarp-2.40.1.tar.gz ." % cwd)
+            _run_shell("tar -xzvf swarp-2.40.1.tar.gz")
+            with pushd("swarp-2.40.1"):
+                _run_shell("./configure --prefix=%s" % tmpdir)
+                _run_shell("make")
+                _run_shell("make install")
+            _run_shell("cp bin/swarp %s/eastlake/astromatic/." % cwd)
+
+
+class build_py(setuptools.command.build_py.build_py):
+    def run(self):
+        _build_swarp()
+        super().run()
+
+
+class build_ext(setuptools.command.build_ext.build_ext):
+    def run(self):
+        _build_swarp()
+        super().run()
+
 
 scripts = glob.glob('./bin/*')
 scripts = [os.path.basename(f) for f in scripts if f[-1] != '~']
@@ -20,4 +67,11 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     scripts=scripts,
+    cmdclass={
+        'build_py': build_py,
+        'build_ext': build_ext,
+    },
+    package_data={
+        "eastlake": ["astromatic/*"],
+    }
 )
