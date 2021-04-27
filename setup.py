1d3339173689f68360b7edf99ceabdc79b23d77a
Matthew R. Becker <becker.mr@gmail.com>
2021-04-27 11:24:26 -0500

    ENH try linux build

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ import tempfile
 import contextlib
 import subprocess
 import glob
+import sys
+
+
+if "--skip-swarp-build" in sys.argv:
+    SKIP_SWARP_BUILD = True
+    sys.argv.remove("--skip-swarp-build")
+else:
+    SKIP_SWARP_BUILD = False
+
+
 from setuptools import setup, find_packages
 import setuptools.command.build_ext
 import setuptools.command.build_py
@@ -25,6 +35,8 @@ def _run_shell(cmd):
 
 
 def _build_swarp():
+    if SKIP_SWARP_BUILD:
+        return
     cwd = os.path.abspath(os.getcwd())
     with tempfile.TemporaryDirectory() as tmpdir:
         with pushd(tmpdir):
@@ -37,15 +49,75 @@ def _build_swarp():
             _run_shell("cp bin/swarp %s/eastlake/astromatic/." % cwd)
 
 
+def _build_src_ext():
+    if "CONDA_BUILD" in os.environ:
+        cc = "${CC}"
+        cldflags = "\"${CFLAGS} ${LDFLAGS}\""
+    elif all(v in os.environ for v in ["CONDA_PREFIX", "CC", "CFLAGS", "LDFLAGS"]):
+        cc = os.environ["CC"]
+        cldflags = "\"" + os.environ["CFLAGS"] + " " + os.environ["LDFLAGS"] + "\""
+    elif sys.platform == "linux":
+        cc = "gcc"
+        cldflags = (
+            "\"-isystem ${CONDA_PREFIX}/include "
+            "-Wl,-rpath,${CONDA_PREFIX}/lib "
+            "-Wl,-rpath-link,${CONDA_PREFIX}/lib "
+            "-L${CONDA_PREFIX}/lib\""
+        )
+    elif sys.platform == "darwin":
+        cc = "clang"
+        cldflags = (
+            "\"-ftree-vectorize -fPIC -fPIE -fstack-protector-strong -O2 -pipe "
+            "-isystem ${CONDA_PREFIX}/include "
+            "-Wl,-rpath,${CONDA_PREFIX}/lib "
+            "-L${CONDA_PREFIX}/lib "
+            "-Wl,-pie -Wl,-headerpad_max_install_names "
+            "-Wl,-dead_strip_dylibs\""
+        )
+    else:
+        print(sys.platform, flush=True)
+        raise RuntimeError("platform %s not recognized" % sys.platform)
+
+    cwd = os.path.abspath(os.getcwd())
+    with tempfile.TemporaryDirectory() as tmpdir:
+        with pushd(tmpdir):
+            _run_shell("cp %s/src/src-extractor-2.24.4.tar.gz ." % cwd)
+            _run_shell("tar -xzvf src-extractor-2.24.4.tar.gz")
+            with pushd("sextractor-2.24.4"):
+                try:
+                    _run_shell("sh autogen.sh")
+                    _run_shell(
+                        "CC=%s "
+                        "CFLAGS=%s "
+                        "./configure "
+                        "--prefix=%s "
+                        "--enable-openblas "
+                        "--with-openblas-libdir=${CONDA_PREFIX}/lib "
+                        "--with-openblas-incdir=${CONDA_PREFIX}/include " % (
+                            cc, cldflags, tmpdir
+                        )
+                    )
+                except Exception:
+                    _run_shell("cat config.log")
+                    raise
+
+                _run_shell("make")
+                _run_shell("make install")
+
+            _run_shell("cp bin/sex %s/eastlake/astromatic/src-extractor" % cwd)
+
+
 class build_py(setuptools.command.build_py.build_py):
     def run(self):
         _build_swarp()
+        _build_src_ext()
         super().run()
 
 
 class build_ext(setuptools.command.build_ext.build_ext):
     def run(self):
         _build_swarp()
+        _build_src_ext()
         super().run()
 
 
