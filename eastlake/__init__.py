9cd086edee6f15b0f713555a1fc527f8cab493a5
Matthew R. Becker <becker.mr@gmail.com>
2021-06-04 06:59:16 -0500

    ENH move to version from tags

--- eastlake/__init__.py
+++ eastlake/__init__.py
@@ -1,3 +1,13 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("eastlake")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
 from .pipeline import register_pipeline_step  # noqa
 from . import des_piff  # noqa
-from ._version import __version__  # noqa
--- setup.py
+++ setup.py
@@ -160,17 +160,8 @@ scripts = glob.glob('./bin/*')
 scripts = [os.path.basename(f) for f in scripts if f[-1] != '~']
 scripts = [os.path.join('bin', s) for s in scripts]
 
-__version__ = None
-pth = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    "eastlake",
-    "_version.py")
-with open(pth, 'r') as fp:
-    exec(fp.read())
-
 setup(
     name='eastlake',
-    version=__version__,
     packages=find_packages(),
     include_package_data=True,
     scripts=scripts,
@@ -180,5 +171,7 @@ setup(
     },
     package_data={
         "eastlake": ["astromatic/*"],
-    }
+    },
+    use_scm_version=True,
+    setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
 )
